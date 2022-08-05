c16be8409ab304c9d1dcfc508fd6e83147443344
Kevin <kevinkoeks05@gmail.com>
2022-08-06 01:43:51 +0200

    added package+code

---
+++ employeeManager/src/main/java/com/kevcompany/employeeManager/model/Employee.java
@@ -0,0 +1,15 @@
+package com.kevcompany.employeeManager.model;
+
+import java.io.Serializable;
+
+public class Employee implements Serializable {
+    private Long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private int phone;
+    private String imageUrlLocation;
+
+
+
+}
