baebe921071184a7c6f243a17b486015174bd2da
Kevin <kevinkoeks05@gmail.com>
2022-08-13 20:55:44 +0200

    Finished Service options

--- employeeManager/employeeManager/src/main/java/com/kevcompany/employeeManager/service/EmployeeService.java
+++ employeeManager/employeeManager/src/main/java/com/kevcompany/employeeManager/service/EmployeeService.java
@@ -5,6 +5,7 @@ import com.kevcompany.employeeManager.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -20,4 +21,22 @@ public class EmployeeService {
         employee.setCodeForEmployee(UUID.randomUUID().toString()); //Generates Random ID
         return employeeRepository.save(employee);
     }
+
+    public List<Employee> findAllTheEmployee() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+//    public Employee findTheEmployeeById(Long id) {
+//        return employeeRepository.findTheEmployeeById(id).orElseThrow( () -> new UserNotFoundException("User by the id: " + id + " was not found"));
+//    }
+
+    public void deleteTheEmployee(Long id) {
+        employeeRepository.deleteEmployeeById(id);
+    }
+
+
 }
